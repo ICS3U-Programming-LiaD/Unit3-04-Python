bc808d948f4980a7e93fa8a26192eb653963e977
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-10-12 00:20:18 +0000

    Tells you if the integer is positive, negative or zero

---
+++ integer.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env python3
+# Created by: Lia Duggan
+# Created on: October 11th 2022
+# This program tells you if the given integer is 
+#positive, negative or zero
+
+#Asking user to input an Integer
+user_integer = int(input ("Enter an Integer:  "))
+print("") 
+
+#Determining whether the Integer is Positive, negative or Zero
+
+if user_integer >0:
+    print(user_integer," is a positive number")
+elif user_integer <0:
+    print(user_integer, " is a negative number")
+else:
+    print(user_integer, " is just zero")
