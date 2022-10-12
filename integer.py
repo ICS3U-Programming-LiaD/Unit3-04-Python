69af5e39143a2592c81def5f2577eed89b169736
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-10-12 18:11:45 +0000

    tells user if the integer they entered is positive, negative or zero

--- integer.py
+++ integer.py
@@ -3,17 +3,20 @@
 # Created by: Lia Duggan
 # Created on: October 11th 2022
 # This program tells you if the given integer is 
-#positive, negative or zero
+# positive, negative or zero
 
-#Asking user to input an Integer
-user_integer = int(input ("Enter an Integer:  "))
-print("") 
+def main():
+    #Asking user to input an Integer
+    user_integer = int(input ("Enter an Integer: "))
+    print("") 
 
-#Determining whether the Integer is Positive, negative or Zero
+    #Determining whether the Integer is Positive, negative or Zero
+    if user_integer >0:
+        print(user_integer," is a positive number")
+    elif user_integer <0:
+        print(user_integer, " is a negative number")
+    else:
+        print(user_integer, " is just zero")
 
-if user_integer >0:
-    print(user_integer," is a positive number")
-elif user_integer <0:
-    print(user_integer, " is a negative number")
-else:
-    print(user_integer, " is just zero")
+if __name__ == "__main__":
+    main()
